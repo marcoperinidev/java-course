9c06622491eb64eee4614d9333c85fb742c79b14
marcoperinidev <info@mpdev.me>
2023-02-25 02:18:58 +0100

    aggiunti validazione login,bottoni e welcome page

--- PersonalProjects/LoginSystem/LoginSystem/src/LoginPage.java
+++ PersonalProjects/LoginSystem/LoginSystem/src/LoginPage.java
@@ -16,7 +16,8 @@ public class LoginPage implements ActionListener {
 
     JLabel userIDLabel = new JLabel("userID:");
     JLabel userPasswordLabel = new JLabel("password:");
-    JLabel messageLabel = new JLabel("this is a test");
+    JLabel messageLabel = new JLabel();
+
 
     HashMap<String,String> loginInfo = new HashMap<String,String>();
     LoginPage(HashMap<String,String> loginInfoOriginal) {
@@ -28,9 +29,25 @@ public class LoginPage implements ActionListener {
         messageLabel.setBounds(125,250,250,35);
         messageLabel.setFont(new Font(null, Font.ITALIC, 25));
 
+        userIDField.setBounds(125,100,200,25);
+        userPasswordField.setBounds(125,150,200,25);
+
+        loginButton.setBounds(125,200,100,25);
+        loginButton.setFocusable(false);
+        loginButton.addActionListener(this);
+
+        resetButton.setBounds(225,200,100,25);
+        resetButton.setFocusable(false);
+        resetButton.addActionListener(this);
+
+
         frame.add(userIDLabel);
         frame.add(userPasswordLabel);
         frame.add(messageLabel);
+        frame.add(userIDField);
+        frame.add(userPasswordField);
+        frame.add(loginButton);
+        frame.add(resetButton);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420,420);
         frame.setLayout(null);
@@ -39,6 +56,33 @@ public class LoginPage implements ActionListener {
 
 
     public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource() == resetButton) {
+            userIDField.setText("");
+            userPasswordField.setText("");
+        }
+
+        if(e.getSource() == loginButton) {
+            String userID = userIDField.getText();
+            String password = String.valueOf(userPasswordField.getPassword());
+
+            if(loginInfo.containsKey(userID)) {
+                if(loginInfo.get(userID).equals(password)) {
+                    messageLabel.setForeground(Color.green);
+                    messageLabel.setText("Login successful");
+                    frame.dispose();
+                    WelcomePage welcomePage = new WelcomePage(userID);
+                }
+                else {
+                    messageLabel.setForeground(Color.red);
+                    messageLabel.setText("Wrong password");
+                }
+            }
+            else {
+                messageLabel.setForeground(Color.red);
+                messageLabel.setText("Wrong userID");
+            }
+        }
 
     }
 }
--- PersonalProjects/LoginSystem/LoginSystem/src/WelcomePage.java
+++ PersonalProjects/LoginSystem/LoginSystem/src/WelcomePage.java
@@ -1,2 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class WelcomePage {
+
+    JFrame frame = new JFrame();
+    JLabel welcomeLabel = new JLabel("Welcome");
+    WelcomePage(String userID) {
+
+        welcomeLabel.setBounds(0,0,200,35);
+        welcomeLabel.setFont(new Font(null, Font.PLAIN, 25));
+        welcomeLabel.setText("Welcome " + userID);
+        welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
+        welcomeLabel.setVerticalAlignment(JLabel.CENTER);
+
+
+        frame.add(welcomeLabel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420,420);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
 }
