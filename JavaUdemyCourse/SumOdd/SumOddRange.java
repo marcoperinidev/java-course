2c29a8cdfd3079ab03ca8881c0293e4cf7235caf
marcoperinidev <info@mpdev.me>
2023-03-09 10:28:01 +0100

    created Main class to run and docker try

--- JavaUdemyCourse/SumOdd/SumOddRange.java
+++ JavaUdemyCourse/SumOdd/SumOddRange.java
@@ -2,11 +2,7 @@ public class SumOddRange {
     public static boolean isOdd(int number) {
         if (number < 0) {
             return false;
-        } else if (number % 2 == 0) {
-            return false;
-        } else {
-            return true;
-        }
+        } else return number % 2 != 0;
     }
 
     public static int sumOdd(int start, int end) {
